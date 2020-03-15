e5434d68997638a915f368293ffa8fee181cb873
Enrique Pittaluga <ep7m@me.com>
2020-03-15 16:07:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,7 +64,6 @@ def runner
   welcome()
   card_total = initial_round()
   while (card_total <= 21) do 
-    prompt_user()
     card_total = hit?(card_total)
     display_card_total(card_total)
   end 
